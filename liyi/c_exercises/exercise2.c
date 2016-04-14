d6f5dd8efacffdc70e656ef78a734e9bbc91a402
LiYi5 <675204903@qq.com>
2016-04-14 21:50:05 +0800

    2 exercise

---
+++ liyi/c_exercises/exercise2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*@Author:liyi
+  @Brief:statements by using printf function to print the asterisk pattern
+  @created:14/4/06*/
+int main(int argc,char*argv[]) 
+{printf("Welcome!\n");
+printf("You are able to test your skill of writing C code here.\n");
+system("PAUSE");
+return 0;
+}
