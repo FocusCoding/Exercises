f359cc7680c4fb400861b4438617b1b95ae58bb7
BiYouyue <1144519351@qq.com>
2016-04-14 17:40:25 +0800

    second exercises

---
+++ biyouyue/c-exercises/C variables and datatypes exercises/exercise1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/********************************************************   
++* @Author: biyouyue 
++* @Brief:  Write a C program to print the following line.
+    Welcome!
+  	You are able to test your skill of writing C code here
++* @CreatedTime: 14/4/16.  
++*********************************************************/
+int main(int argc,char*argv[])
+{
+	printf("Welcome!\n");
+	printf("You are able to test your skill of writing C code here.\n");
+
+	system("PAUSE");
+	return 0;
+}
