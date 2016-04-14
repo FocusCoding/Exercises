85550d89aad9870f6ef6b42e20993006501aa409
LiYi5 <675204903@qq.com>
2016-04-14 18:54:24 +0800

    frist exercise

---
+++ liyi/c_exercises/exercise1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/***@LIYI
+ @This is a C grammer.
+  @14/4/16***/ 
+int main(int argc,char*argv[])
+{
+	printf("Welcome£¡\n");
+	printf("You are able to test your skill of writing C code here,\n");
+	system("PAUSE");
+	return 0;
+}
+ 
